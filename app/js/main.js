543df127f3e6bb50b74986e1183eefa395ebdbae
pjupju <tkachenkoyuriy12@gmail.com>
2020-09-08 19:29:56 +0300

    add chrome version

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,43 @@
 
 $(document).ready(function(){
-   
+    $('.slider__items').slick({
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        /*
+        autoplay: true,
+        autoplaySpeed: 3000, */
+        variableWidth: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '30px',
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '20px',
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 380,
+            settings: {
+                centerPadding: '22px',
+            }
+          }
+        ]
+      });
     
 })
